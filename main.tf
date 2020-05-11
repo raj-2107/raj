805341149a92cabcc04a7c194dc7be75e386dbd6
root <root@ip-172-31-32-19.ap-south-1.compute.internal>
2020-05-11 07:43:45 +0000

    modified main

--- main.tf
+++ main.tf
@@ -53,10 +53,10 @@ resource "aws_launch_configuration" "example" {
 ## AutoScaling Group
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.id
-  availability_zones = ["data.aws_availability_zones[count.index]"]
+  availability_zones = ["ap-south-1a","ap-south-1b"]
   min_size = 2
   max_size = 4
-  load_balancers = ["aws_elb.example.name"]
+  load_balancers = [aws_elb.example.name]
   health_check_type = "ELB"
   tag {
     key = "Name"
